cba7759c228ee5c005cb1987341c56177ae3c14a
Hidaka Masatoshi <hidaka@mi.t.u-tokyo.ac.jp>
2016-11-23 17:25:34 +0900

    display cell id

--- docs/js/sushilab.js
+++ docs/js/sushilab.js
@@ -16,25 +16,37 @@
       var input_script = textarea_cell.val();
 
       var div_cell = textarea_cell.parents("div.cell");
+      var cell_id = Number(div_cell.data('cell-id'));
       var div_result = $('<div class="result"></div>');
+      div_result.data('cell-id', cell_id.toString());
       div_cell.after(div_result);
+      var div_result_body = $('<div class="result-body"></div>');
+
       try {
         var result = ('global', eval)(input_script);//eval in global scope
         //div_result.text(result);
-        print_on_result_cell(div_result, result);
+        print_on_result_cell(div_result_body, result);
       } catch (error) {
-        div_result.addClass('error-cell');
-        div_result.text(error.toString());
+        div_result_body.addClass('error-cell');
+        div_result_body.text(error.toString());
       }
+      div_result.append('<span class="cell-id">Out ' + cell_id + '</span>');
+      div_result.append(div_result_body);
 
       add_cell();
       return false;
     }
   });
 
+  var next_cell_id = 1;
   function add_cell() {
+    var cell_id = next_cell_id;
+    next_cell_id++;
     var textarea = $('<textarea class="cell" rows="4" cols="80"></textarea>');
-    var cell = $('<div class="cell"></div>');
+    var cell = $('<div class="cell" data-cell-id="' + cell_id + '"></div>');
+    var cell_id_span = $('<span class="cell-id"></span>');
+    cell_id_span.text('In ' + cell_id + '');
+    cell.append(cell_id_span);
     cell.append(textarea);
     $("#cells").append(cell);
     textarea.focus();
