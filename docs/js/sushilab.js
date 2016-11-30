41fdb4a099c670764a8138986c4ed309a6d5bdbd
Hidaka Masatoshi <hidaka@mi.t.u-tokyo.ac.jp>
2016-11-30 21:35:12 +0900

    support run same cell multiple times

--- docs/js/sushilab.js
+++ docs/js/sushilab.js
@@ -7,7 +7,10 @@
     // });
 
     $("#new-notebook").click(function () {
-      window.open(location.href);
+      // remove "loadurl=*" from current url
+      var new_url = location.href;
+      new_url = new_url.replace(/\?loadurl=([^&#]*)/, '');//TODO: inaccurate
+      window.open(new_url);
     });
 
     $("#load-notebook").click(function () {
@@ -26,8 +29,12 @@
       export_notebook(name);
     });
 
-    $("#import-notebook").click(function(e){
+    $("#import-notebook").click(function (e) {
       var name = $("#export-name").val();
+      if (name.length == 0) {
+        alert('Please specify a name to store the notebook in the Name box');
+        return false;
+      }
       var files = $("#import-notebook-file")[0].files;
       if (files.length > 0) {
         var f = files[0];
@@ -46,41 +53,71 @@
       sessionStorage.removeItem('load_name');
       console.log('loading notebook ' + load_name);
       load_notebook(load_name);
+    } else {
+      var load_regex = new RegExp("[\\?&]loadurl=([^&#]*)");
+      var load_regex_match = load_regex.exec(window.location.href);
+      if (load_regex_match) {
+        loadurl_notebook(load_regex_match[1]);
+      } else {
+        add_cell(true);
+      }
     }
-    add_cell();
   });
 
   $(document).on('keydown', 'textarea.cell', function (e) {
     if (e.shiftKey && e.keyCode === 13) {
       var textarea_cell_dom = this;
       var textarea_cell = $(textarea_cell_dom);
-      var input_script = textarea_cell.val();
 
       var div_cell = textarea_cell.parents("div.cell");
       var cell_id = Number(div_cell.data('input-cell-id'));
-      var div_result = $('<div class="result"></div>');
-      div_result.attr('data-output-cell-id', cell_id.toString());
-      div_cell.after(div_result);
-      var div_result_body = $('<div class="result-body"></div>');
-
-      try {
-        var result = ('global', eval)(input_script);//eval in global scope
-        //div_result.text(result);
-        print_on_result_cell(div_result_body, result);
-      } catch (error) {
-        div_result_body.addClass('error-cell');
-        div_result_body.text(error.toString());
-      }
-      div_result.append('<span class="output-cell-id">Out ' + cell_id + '</span>');
-      div_result.append(div_result_body);
-
-      add_cell();
+      exec_cell(cell_id);
       return false;
     }
   });
 
+  function exec_cell(cell_id) {
+    var input_cell_div = $("div[data-input-cell-id='" + cell_id + "']");
+    var textarea_cell = input_cell_div.children("textarea");
+    var input_script = textarea_cell.val();
+    input_cell_div.addClass('executedcell');
+
+    var div_result = $("div[data-output-cell-id='" + cell_id + "']");
+    var new_result_cell = false;
+    if (div_result.length == 0) {
+      // create new result cell
+      new_result_cell = true;
+      div_result = $('<div class="result"></div>');
+      div_result.attr('data-output-cell-id', cell_id.toString());
+      input_cell_div.after(div_result);
+    } else {
+      // empty and reuse existing cell
+      div_result.empty();
+    }
+    var div_result_body = $('<div class="result-body"></div>');
+
+    try {
+      var result = ('global', eval)(input_script);//eval in global scope
+      //div_result.text(result);
+      print_on_result_cell(div_result_body, result);
+    } catch (error) {
+      div_result_body.addClass('error-cell');
+      div_result_body.text(error.toString());
+    }
+    div_result.append('<span class="output-cell-id">Out ' + cell_id + '</span>');
+    div_result.append(div_result_body);
+
+    if (new_result_cell) {
+      add_cell(focus);
+    } else {
+      // focus on next cell
+      $("div[data-input-cell-id='" + (cell_id + 1) + "']").children("textarea").focus();
+    }
+
+  }
+
   var next_cell_id = 1;
-  function add_cell() {
+  function add_cell(focus) {
     var cell_id = next_cell_id;
     next_cell_id++;
     var textarea = $('<textarea class="cell" rows="4" cols="80"></textarea>');
@@ -90,7 +127,9 @@
     cell.append(cell_id_span);
     cell.append(textarea);
     $("#cells").append(cell);
-    textarea.focus();
+    if (focus) {
+      textarea.focus();
+    }
   }
 
   function onerror_function(msg, url, lineNo) {
@@ -129,8 +168,26 @@
       alert("Notebook not found");
       return false;
     }
-    var import_data = JSON.parse(import_json);
-    var cells = import_data['cells'];
+    var notebook_obj = JSON.parse(import_json);
+    parse_notebook_json(notebook_obj);
+    add_cell(false);
+    $("div[data-input-cell-id='1']").children("textarea").focus();
+    return true;
+  }
+
+  function loadurl_notebook(url) {
+    var loading_text = $('<div>Loading notebook from network...</div>');
+    $("#cells").append(loading_text);
+    $.getJSON(url, function (notebook_obj) {
+      loading_text.remove();
+      parse_notebook_json(notebook_obj);
+      add_cell(false);
+      $("div[data-input-cell-id='1']").children("textarea").focus();
+    });
+  }
+
+  function parse_notebook_json(notebook_obj) {
+    var cells = notebook_obj['cells'];
 
     for (var cell_id = 1; ; cell_id++) {
       var cell_data = cells[cell_id];
@@ -150,7 +207,6 @@
       $("#cells").append($(cell_data["output_html"]));
     }
 
-    return true;
   }
 
   function save_notebook(name) {
